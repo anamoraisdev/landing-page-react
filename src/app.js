5336ae97e3d3155cf7d23e54b9e336fd253961e9
jonathastavares <jonathastavares@id.uff.br>
2022-10-30 14:07:03 -0300

    Newsletter section created

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import Description from './components/description';
+import Newsletter from './components/newsletter';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Description />
+      <Newsletter />
     </div>
   );
 };
